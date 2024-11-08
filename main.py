6cda6f469f07036fa84c6812391d6884a91d193b
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-11-08 18:07:14 +0800

    Save changes to main.py before pulling

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ import DCT.dctDecode as dctDecode
 import EOI.jpegeoi as eoi
 
 
+
+
+# Large byte sequence to hide
+secret_data = b'p..?\xa8\x8f\xf3M\x0e3\x18\xb8;Ml\xef\x10\x160h\xd4\xfb\x1aO\x96\xa0VO^\xb0^\xe8\x8a\xc2\xca\x9b\xe8\xba\xbc\x17?P\xea\xed\x0eK:\xa1\x8a\xc7\x01\xc9\xd9F\x16\xef}\xf1\xc9)\xfc\xfd\xc5d\x97<\tA\xe7I\xe6\xf4\x97\x8b\x95\x9f\x8c\\`d\xe0NC\x0b\xe9\xc6}\xf8\xf8\xd3}\xf1Y\xb2\x14\xc4{G9\xb6\xa0x|\x92\x0f\xb3\xfb\xa4\xc3\x11\x94\xcb_\xbdSP\xd8tF\xae\xf3i6h\x02\x0f\xa4\xce\xd2\xda\x8d\x0c!b:\x8eQ\xd7+L\xee\xfd\xb6=X\xb8w\xb2Z\x03n\xab3%\xa4\x8c\x03%e\xc3\xb7XRo)\x9d\xb9\xf4\xfdx\xd2\x90N<x\xbc\xcaMh[\xf4\x8b\xbc{G\x14\x83\xe81\x8a0\\\\\x9d\x97V\xfcI\xe1\x82\xf7\xf3\xdcc\x9a\x88\x8b\xe6L\x1d\xbf\xd6w\x0c~<k\x0c\xe9\xbe\xfcb{$\x87\xbfo\x86n\xd4\xbbY\x8c\xaa\xc4|Gh\xe96/\xe3k\xab\xd77w\xc6o\xa5\xc5^\x1cL\xb0\xfe\xf5\xa7Sg\'E\xb3\x87C3\xdf\xfe\xf2X.\x1f\xe5i\xbc\x94\xb0\x82\x99\xef\xc7\xb3\xf07\xef\xe4R\x10\xed\xda }E\xb4*|\xd5\xc0\xeb)\xc2\xd3\xcf;\x8e\xf1\x96,\x04Y\x14\xa49L\xb2J\x1f\x1e34\xfb\xff\xe8\xfd\xd3{\x94}+13\\\xc1\x07Nb\x13n\x7f\xab\x04\x7f\x00\xbe\x94F\xa3\x11\xb3\xe1r\x10\x1f\x89\xd9\xf5:\x96\xba\xe4\xd7\n=\xf8`\xc5\x8b\xf4jei\xc8\x04N\x13\xd3\x19\x7f\x01\x9f\x98\x12`9\xe5\x18b\x0e\xb6et)\xbd\x1fjG\x00\x8c\x06K\xae\xf1\xc0\xc5\x9157\xb9\x95e\xa8y\xf4r\xfb\xab\x9d\xc1V\xafG\xbd2C\xfe\xcaQ\x10\'?\xbaA\x08\x86\x80\xdb6\xbc\x05\x02\xb9\xe9]q\x17o\x9c\xccP\xc7\x89\xac\xfdy\x04&5\xa5\x8f\x08\x11_\xae\xd6\x7fQ\xf9\xae\xaf\xfa^\xc3<\xce\xbc$\xd9\xa9D\xfa\x16\x91q\x19\xf2\xb3=\x88`\x1d\xcen\xb1|"|o\xe5O\xd4\xad\x02\xa7\xf6\t\xd0#+\x1f>n'
+#secret_data = b'\xa8\x8f\xf3M\x0e3\x18\xb8;'
+# Get data length in bytes
+data_length = len(secret_data)
+print("Data length (in bytes):", data_length)
+
+
+
 REG_PATH = r"SOFTWARE\f3832454-4e14-a1b9-0f614e507aa5"
 AES_KEY_SIZE = 32  # 256 bits for AES-256
 IV_SIZE = 16
@@ -165,17 +176,27 @@ def hide_mode(steg_technique, path_of_data, number_of_files):
     
     splitted_data = split_byte_data(ciphertext, number_of_files)
 
+    # Retrieve available files
+    list_of_used_files = [file for file in path.iterdir() if file.is_file()]
+
+    # Ensure `list_of_used_files` is limited to the number of data chunks
+    list_of_used_files = list_of_used_files[:len(splitted_data)]
+
 
     if steg_technique.lower() == "lsb":
-        # List comprehension to retrieve file names
-        list_of_used_files = [file.name for file in path.iterdir() if file.is_file() and file.suffix in ['jpg']]
-        list_of_used_files = list_of_used_files[:len(splitted_data)]
-        if len(list_of_used_files) != len(splitted_data):
-            print(f"Not enough images to be used. Number of images needed is {len(splitted_data)}.")
+        # Only use PNG files for LSB embedding
+        png_files = [file for file in list_of_used_files if file.suffix.lower() == '.png']
+        print("Data length (in bytes):", len(secret_data))
+        # Ensure we have enough PNG files for the data chunks
+        if len(png_files) < len(splitted_data):
+            print(f"Not enough PNG images to hide data. Required: {len(splitted_data)}, found: {len(png_files)}.")
             sys.exit(1)
-        for i in range(len(splitted_data)):
-            data = splitted_data[i]
-            file_path = str(path.absolute())+'\\'+ list_of_used_files[i]
+
+        # Embed each chunk of data in separate PNG files using LSB
+        for i, data_chunk in enumerate(splitted_data):
+            file_path = png_files[i]
+            lsb.hide_data_in_png(str(file_path), data_chunk)
+            print(f"Data chunk embedded in {file_path}")
             # Hide the encrypted data in the image
             # hide_data_in_jpg(file, )
 
@@ -241,11 +262,11 @@ def hide_mode(steg_technique, path_of_data, number_of_files):
 
 
     with open('order.txt', "w") as file:
-        for i, files in enumerate(list_of_used_files):
+        for i, file_path in enumerate(list_of_used_files):
             if i < len(list_of_used_files) - 1:
-                file.write(files + ',')
+                file.write(str(file_path) + ',')  # Convert Path to string
             else:
-                file.write(files)
+                file.write(str(file_path))  # Convert Path to string
         file.close()
 
 
@@ -253,7 +274,7 @@ def hide_mode(steg_technique, path_of_data, number_of_files):
     # print(f"Data from {file_path} has been hidden and the original file securely wiped.")
 
 
-def unhide_mode():
+def unhide_mode(file_list_path, data_length):
     pass
 
 def wipe_file(file_path):
